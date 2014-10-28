52c00a96756f48da7cd99d9a8d7f342008caa4e8
lipido <dgpena@uvigo.es>
2014-10-28 16:57:49 +0100

    Align text

--- index.php
+++ index.php
@@ -11,28 +11,28 @@ define("DEFAULT_CONTROLLER", "posts");
  */
 define("DEFAULT_ACTION", "index");
 
- /**
-  * Main router (single entry-point for all requests)
-  * of the MVC implementation.
-  * 
-  * This router will create an instance of the corresponding
-  * controller, based on the "controller" parameter and call
-  * the corresponding method, based on the "action" parameter.
-  * 
-  * The rest of GET or POST parameters should be handled by
-  * the controller itself.
-  * 
-  * Parameters:
-  * <ul>
-  * <li>controller: The controller name (via HTTP GET)
-  * <li>action: The name inside the controller (via HTTP GET)
-  * </ul>
-  * 
-  * @return void
-  * 
-  * @author lipido <lipido@gmail.com>
-  */
- function run() {
+/**
+ * Main router (single entry-point for all requests)
+ * of the MVC implementation.
+ * 
+ * This router will create an instance of the corresponding
+ * controller, based on the "controller" parameter and call
+ * the corresponding method, based on the "action" parameter.
+ * 
+ * The rest of GET or POST parameters should be handled by
+ * the controller itself.
+ * 
+ * Parameters:
+ * <ul>
+ * <li>controller: The controller name (via HTTP GET)
+ * <li>action: The name inside the controller (via HTTP GET)
+ * </ul>
+ * 
+ * @return void
+ * 
+ * @author lipido <lipido@gmail.com>
+ */
+function run() {
   // invoke action!
   try {
     if (!isset($_GET["controller"])) {
@@ -50,34 +50,34 @@ define("DEFAULT_ACTION", "index");
     //uniform treatment of exceptions
     die("An exception occured!!!!!".$ex->getMessage());   
   }
- }
+}
  
- /**
-  * Load the required controller file and create the controller instance
-  * 
-  * @param string $controllerName The controller name found in the URL
-  * @return Object A Controller instance
-  */
- function loadController($controllerName) {  
+/**
+ * Load the required controller file and create the controller instance
+ * 
+ * @param string $controllerName The controller name found in the URL
+ * @return Object A Controller instance
+ */
+function loadController($controllerName) {  
   $controllerClassName = getControllerClassName($controllerName);
   
   require_once(__DIR__."/controller/".$controllerClassName.".php");  
   return new $controllerClassName();
- }
+}
  
- /**
-  * Obtain the class name for a controller name in the URL
-  * 
-  * For example $controllerName = "users" will return "UsersController"
-  * 
-  * @param $controllerName The name of the controller found in the URL
-  * @return string The controller class name
-  */
- function getControllerClassName($controllerName) {
+/**
+ * Obtain the class name for a controller name in the URL
+ * 
+ * For example $controllerName = "users" will return "UsersController"
+ * 
+ * @param $controllerName The name of the controller found in the URL
+ * @return string The controller class name
+ */
+function getControllerClassName($controllerName) {
   return strToUpper(substr($controllerName, 0, 1)).substr($controllerName, 1)."Controller";
- }
+}
  
- //run!
- run();
+//run!
+run();
  
- ?>
+?>
