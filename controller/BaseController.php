19a3faf4264abf5307df2a8f02657738e4cdd61d
lipido <dgpena@uvigo.es>
2014-10-31 12:13:12 +0100

    Remove duplicated require_once

--- controller/BaseController.php
+++ controller/BaseController.php
@@ -3,7 +3,6 @@
 
 require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../core/I18n.php");
-require_once(__DIR__."/../core/ViewManager.php");
 
 require_once(__DIR__."/../model/User.php");
 
