990e5d6dc0e2d5abceebcb8a7cbc8bc9ed4f12a3
lipido <dgpena@uvigo.es>
2015-10-27 22:03:46 +0100

    Improve documentation

--- core/ViewManager.php
+++ core/ViewManager.php
@@ -4,23 +4,38 @@
 /**
  * Class ViewManager
  *
- * This class implements a basic layout system based
- * on PHP output buffers (ob_ functions). This class is 
- * a singleton. You should use ViewManager::getInstance() 
- * to get the view manager.
+ * This class implements the glue between the controller
+ * and the view.
  *
- * Once the view manager is initialized, the output buffer
- * is enabled. 
- * By default, all contents that are generated inside your views
- * will be saved in a DEFAULT_FRAGMENT. The DEFAULT_FRAGMENT
- * is normally used as the "main" content of the resulting layout
- * However, you can generate contents for other fragments that will
- * go into the layout. For example, inside your views, you have to call 
- * ViewManager::moveToFragment(fragmentName) before generating content 
- * for a desired fragment. This fragment normally will be after
- * retrieved by the layout (which calls to ViewManager::getFragment).
- * Typical fragments are 'css', 'javascript', so you can specify
- * additional css and javascripts from your specific views.
+ * This class is a singleton. You should use getInstance()
+ * to get the view manager instance.
+ *
+ * The main responsibilities are:
+ *
+ * 1.Save variables from the controller and make them available
+ *   for views. This includes "flash" variables, which are
+ *   variables kept in session that are removed just after
+ *   they are retrieved. Use methods like setVariable, getVariable and
+ *   setFlash.
+ *
+ * 2.Render views. This basically performs an 'include' of the view
+ *   file, but with more MVC-oriented parameters
+ *   (controller name and view name).
+ *
+ * 3.Layout (or templating) system. Based on PHP output buffers 
+ *   (ob_ functions). Once the view manager is initialized, 
+ *   the output buffer is enabled. By default, all contents that are 
+ *   generated inside your views will be saved in a DEFAULT_FRAGMENT. 
+ *   The DEFAULT_FRAGMENT is normally used as the "main" content of 
+ *   the resulting layout. However, you can generate contents for 
+ *   other fragments that will go into the layout. For example, inside 
+ *   your views, you have to call moveToFragment(fragmentName) before 
+ *   generating content for a desired fragment. This fragment normally 
+ *   will be after retrieved by the layout (via calls to getFragment).
+ *   Typical fragments are 'css', 'javascript', so you can specify
+ *   additional css and javascripts from your specific views.
+ *
+ * @author lipido <lipido@gmail.com>
  */
 class ViewManager {
   
