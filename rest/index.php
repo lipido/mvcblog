6cee38dc4b973b5db8e6ec2f57808e2c187633da
lipido <dgpena@uvigo.es>
2017-11-21 12:30:46 +0100

    Improves general error detection and reporting
    We have extended the try-catch block in order to capture errors when
    including *Rest files, which may connect to the database, and thus,
    if the database is not available an uncaught exception is thrown.
    We now also capture these kind of exceptions in our catch block.
    
    We have also improved the error reporting by sending a JSON object
    with an "error" property including the error message.

--- rest/index.php
+++ rest/index.php
@@ -1,18 +1,17 @@
 <?php
 // Simple REST router
 
-require_once(dirname(__FILE__)."/URIDispatcher.php");
-
-// dinamically include Rest files (*Rest.php) in this directory
-$files_in_script_dir = scandir(__DIR__);
-foreach($files_in_script_dir as $filename) {
-	// if filename ends with *Rest.php
-	if (preg_match('/.*REST\\.PHP/', strtoupper($filename))) {
-		include_once(__DIR__."/".$filename);
-	}
-}
-
 try{
+	require_once(dirname(__FILE__)."/URIDispatcher.php");
+
+	// dinamically include Rest files (*Rest.php) in this directory
+	$files_in_script_dir = scandir(__DIR__);
+	foreach($files_in_script_dir as $filename) {
+		// if filename ends with *Rest.php
+		if (preg_match('/.*REST\\.PHP/', strtoupper($filename))) {
+			include_once(__DIR__."/".$filename);
+		}
+	}
 
 	//	error_reporting(E_ERROR);
 	$dispatcher = URIDispatcher::getInstance();
@@ -29,7 +28,8 @@ try{
 
 } catch(Exception $ex) {
 	header($_SERVER['SERVER_PROTOCOL'].' 500 Internal server error');
-	die($ex->getMessage());
+	header("Content-Type: application/json");
+	die(json_encode(array("error" => $ex->getMessage())));
 }
 // debug
 //print_r($_SERVER);
